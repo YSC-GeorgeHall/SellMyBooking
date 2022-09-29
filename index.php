3c9601124acf3d15d12c38bfd8ec8c873aed30aa
theloungefinder <georgeehall@hotmail.co.uk>
2022-09-29 14:55:56 +0100

    created index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
       <style>
          body {
          background-color: #f7f7f7;
-         
          }
          .dropdown:hover .dropdown-menu{
          display: block;
@@ -19,7 +18,6 @@
          .dropdown-menu{
          margin-top: 0;
          }
-         
       </style>
       <script>
          $(document).ready(function(){
@@ -33,44 +31,54 @@
       </script>
    </head>
    <body>
-      <div class="d-flex justify-content-end" style="padding-top: 10px;">
-         <i class="fa-brands fa-facebook" style="padding-right: 5px;"></i>
-         <i class="fa-brands fa-twitter" style="padding-right: 5px;"></i>
-         <i class="fa-brands fa-instagram" style="padding-right: 15px;"></i>
+<!-- socials top right start -->
+      <div class="d-flex justify-content-end" style="padding-top:20px;">
+         <i class="fa-brands fa-facebook fa-2xl " style="padding-right: 5px;"></i>
+         <i class="fa-brands fa-twitter fa-2xl" style="padding-right: 5px;"></i>
+         <i class="fa-brands fa-instagram fa-2xl" style="padding-right: 15px;"></i>
       </div>
+<!-- socials top right end -->
+<!-- centred logo start -->
       <ul class="nav justify-content-center" style="padding-top: 15px;">
          <img src="images/SMB.jpg" alt="logo" style="height:100px;">
-        
       </ul>
+<!-- centred logo end -->
+<!-- Navbar start -->
       <ul class="nav justify-content-center" style="padding-top: 15px;">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav" style="border-top: 1px solid black; border-bottom: 1px solid black;">
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="padding-right: 100px;">Sell a Booking</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="padding-right: 100px;">View Listings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="padding-right: 100px;">Other Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="padding-right: 100px;">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" >Log In</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarContent">
+                  <ul class="navbar-nav" style="border-top: 1px solid black; border-bottom: 1px solid black;">
+                     <li class="nav-item">
+                        <a class="nav-link" href="#" style="padding-right: 100px;">Sell a Booking</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#" style="padding-right: 100px;">View Listings</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#" style="padding-right: 100px;">Other Services</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#" style="padding-right: 100px;">Register</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#" >Log In</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </nav>
+      </ul>
+<!-- navbar end -->
 
-</ul>
+
+
+
+
+
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
    </body>
