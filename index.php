c95296ad820e4609150f0cd431ee7ee9d42e8a9f
George Hall <georgehall@Georges-MacBook-Air.local>
2022-12-06 15:37:20 +0000

    testing again

--- index.php
+++ index.php
@@ -118,9 +118,6 @@
          </nav>
       </ul>
 <!-- navbar end -->
-
-new laptop git test
-
 <!-- Constainer Start --><div class="container">
 <div class="d-flex justify-content-center">
 <h1 class="display-3 style="text-align:="text-align: center;">Have a booking you cant go to?</h1><br>
